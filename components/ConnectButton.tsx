11749966f95a10a98d43f6cfc74ce38c0a709f08
Tomas <mantoketo4@gmail.com>
2024-11-05 16:51:29 +0800

    complete project

--- components/ConnectButton.tsx
+++ components/ConnectButton.tsx
@@ -2,5 +2,5 @@ import { DynamicWidget } from '@dynamic-labs/sdk-react-core'
 import { FC } from 'react'
 
 export const ConnectButton: FC = () => {
-  return <DynamicWidget />
+  return <DynamicWidget/>
 }
